f5dde285cdfe89f4a43e18214d3a3b0e97fa1a9d
jyapujuju <danieldangol@gmail.com>
2016-12-06 16:48:14 +0545

    debug the relationship model

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -2,5 +2,5 @@ class Relationship < ActiveRecord::Base
   belongs_to :follower, class_name: "User"
   belongs_to :followed, class_name: "User"
   validates :follower, :followed, presence:true
-  validates :followed_id, uniqueness: {scope :follower_id}
+  validates :followed_id, uniqueness: {scope: :follower_id}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161125110728) do
+ActiveRecord::Schema.define(version: 20161206101103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,13 @@ ActiveRecord::Schema.define(version: 20161125110728) do
   end
 
   add_index "movies", ["imdbid"], name: "index_movies_on_imdbid", using: :btree
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "reviews", force: :cascade do |t|
     t.integer  "user_id"
